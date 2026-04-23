ee11439b2dd39014673986fd704469dbdccb0aad
stynxshrm <stynxshrm@gmail.com>
2026-04-23 10:22:53 +0200

    updated video_stats

--- video_stats.py
+++ video_stats.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv(dotenv_path=".env")
+
 API_KEY = os.getenv("API_KEY")
-
-
 CHANNEL_HANDLE = "MrBeast"
-
+max_Results = 50
 
 def get_playlist_id(channel_handle):
     try: 
@@ -24,11 +23,42 @@ def get_playlist_id(channel_handle):
         return None
     
 
+def get_video_ids(playlist_id):
+    video_ids = []
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_Results}&playlistId={playlist_id}&key={API_KEY}"
+    pageToken = None
+
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id(CHANNEL_HANDLE)
-    if playlist_id:
-        print(f"Playlist ID for channel '{CHANNEL_HANDLE}': {playlist_id}")
-    else:
-        print("Failed to retrieve the playlist ID.")
+    # if playlist_id:
+    #     print(f"Playlist ID for channel '{CHANNEL_HANDLE}': {playlist_id}")
+    # else:
+    #     print("Failed to retrieve the playlist ID.")
 
-    
+    video_ids = get_video_ids(playlist_id)
+    # print(f"Video IDs in the playlist: {video_ids}")
