ffc94393693cc338da6186f70a5731608657b148
stynxshrm <stynxshrm@gmail.com>
2026-04-23 13:40:11 +0200

    added dockerfile to build airflow docker image

--- video_stats.py
+++ video_stats.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import requests
 import json
 from dotenv import load_dotenv
@@ -53,6 +55,57 @@ def get_video_ids(playlist_id):
         raise e
 
 
+def extract_video_data(video_ids):
+
+    extracted_data = []
+
+    def batch_list(video_id_lst, batch_size):
+        for video_id in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[video_id : video_id + batch_size]
+
+    try:
+        for batch in batch_list(video_ids, max_Results):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["id"]
+                snippet = item["snippet"]
+                contentDetails = item["contentDetails"]
+                statistics = item["statistics"]
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet["title"],
+                    "publishedAt": snippet["publishedAt"],
+                    "duration": contentDetails["duration"],
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None),
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id(CHANNEL_HANDLE)
     # if playlist_id:
@@ -62,3 +115,6 @@ if __name__ == "__main__":
 
     video_ids = get_video_ids(playlist_id)
     # print(f"Video IDs in the playlist: {video_ids}")
+
+    extracted_data = extract_video_data(video_ids)
+    save_to_json(extracted_data)
